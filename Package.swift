698223e4c99681432ee0cbfb03c213f530c7fb78
Steven Fisher <tewha@me.com>
2026-05-27 13:07:03 -0700

    Finish rename.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Hive604Symbols",
+    name: "HiveSymbols",
     platforms: [
         .iOS(.v17),
         .macOS(.v15),
@@ -12,15 +12,15 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "Hive604Symbols",
-            targets: ["Hive604Symbols"]
+            name: "HiveSymbols",
+            targets: ["HiveSymbols"]
         ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Hive604Symbols"
+            name: "HiveSymbols"
         ),
 
     ],
