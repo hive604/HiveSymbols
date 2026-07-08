bab887194f7d9f577dbe9b9f4c879ffcf0ea599d
Steven Fisher <tewha@me.com>
2026-07-08 08:19:52 -0700

    Unwrap nonisolated.

--- Sources/HiveSymbols/SymbolExtensions.swift
+++ Sources/HiveSymbols/SymbolExtensions.swift
@@ -9,61 +9,52 @@
 import SwiftUI
 
 public extension Image {
-    nonisolated
-    init(symbol: Symbol) {
+    nonisolated init(symbol: Symbol) {
         self.init(systemName: symbol.rawValue)
     }
 }
 
 public extension Label where Title == Text, Icon == Image {
-    nonisolated
-    init(_ titleKey: LocalizedStringKey, symbol: Symbol) {
+    nonisolated init(_ titleKey: LocalizedStringKey, symbol: Symbol) {
         self.init(titleKey, systemImage: symbol.rawValue)
     }
 
     @_disfavoredOverload
-    nonisolated
-    init<S>(_ title: S, symbol: Symbol) where S: StringProtocol {
+    nonisolated init<S>(_ title: S, symbol: Symbol) where S: StringProtocol {
         self.init(String(title), systemImage: symbol.rawValue)
     }
 
     @_disfavoredOverload
-    nonisolated
-    init(_ titleResource: LocalizedStringResource, symbol: Symbol) {
+    nonisolated init(_ titleResource: LocalizedStringResource, symbol: Symbol) {
         self.init(titleResource, systemImage: symbol.rawValue)
     }
 }
 
 public extension Button where Label == SwiftUI.Label<Text, Image> {
     @_disfavoredOverload
-    nonisolated
-    init<S>(_ title: S, symbol: Symbol, role: ButtonRole? = nil, action: @escaping () -> Void) where S: StringProtocol {
+    nonisolated init<S>(_ title: S, symbol: Symbol, role: ButtonRole? = nil, action: @escaping () -> Void) where S: StringProtocol {
         self.init(String(title), systemImage: symbol.rawValue, role: role, action: action)
     }
 
-    nonisolated
-    init(_ titleKey: LocalizedStringKey, symbol: Symbol, role: ButtonRole? = nil, action: @escaping () -> Void) {
+    nonisolated init(_ titleKey: LocalizedStringKey, symbol: Symbol, role: ButtonRole? = nil, action: @escaping () -> Void) {
         self.init(titleKey, systemImage: symbol.rawValue, role: role, action: action)
     }
 
     @_disfavoredOverload
-    nonisolated
-    init(_ titleResource: LocalizedStringResource, symbol: Symbol, role: ButtonRole? = nil, action: @escaping () -> Void) {
+    nonisolated init(_ titleResource: LocalizedStringResource, symbol: Symbol, role: ButtonRole? = nil, action: @escaping () -> Void) {
         self.init(titleResource, systemImage: symbol.rawValue, role: role, action: action)
     }
 }
 
 public extension NavigationLink where Label == SwiftUI.Label<Text, Image> {
-    nonisolated
-    init(_ titleKey: LocalizedStringKey, symbol: Symbol, @ViewBuilder destination: () -> Destination) {
+    nonisolated init(_ titleKey: LocalizedStringKey, symbol: Symbol, @ViewBuilder destination: () -> Destination) {
         self.init(destination: destination) {
             SwiftUI.Label(titleKey, symbol: symbol)
         }
     }
 
     @_disfavoredOverload
-    nonisolated
-    init<S>(_ title: S, symbol: Symbol, @ViewBuilder destination: () -> Destination) where S: StringProtocol {
+    nonisolated init<S>(_ title: S, symbol: Symbol, @ViewBuilder destination: () -> Destination) where S: StringProtocol {
         let title = String(title)
         self.init(destination: destination) {
             SwiftUI.Label(title, symbol: symbol)
@@ -71,30 +62,26 @@ public extension NavigationLink where Label == SwiftUI.Label<Text, Image> {
     }
 
     @_disfavoredOverload
-    nonisolated
-    init(_ titleResource: LocalizedStringResource, symbol: Symbol, @ViewBuilder destination: () -> Destination) {
+    nonisolated init(_ titleResource: LocalizedStringResource, symbol: Symbol, @ViewBuilder destination: () -> Destination) {
         self.init(destination: destination) {
             SwiftUI.Label(titleResource, symbol: symbol)
         }
     }
 
-    nonisolated
-    init<Value>(_ titleKey: LocalizedStringKey, symbol: Symbol, value: Value?) where Destination == Never, Value: Hashable {
+    nonisolated init<Value>(_ titleKey: LocalizedStringKey, symbol: Symbol, value: Value?) where Destination == Never, Value: Hashable {
         self.init(value: value) {
             SwiftUI.Label(titleKey, symbol: symbol)
         }
     }
 
-    nonisolated
-    init<Value>(verbatim title: String, symbol: Symbol, value: Value?) where Destination == Never, Value: Hashable {
+    nonisolated init<Value>(verbatim title: String, symbol: Symbol, value: Value?) where Destination == Never, Value: Hashable {
         self.init(value: value) {
             SwiftUI.Label(title, symbol: symbol)
         }
     }
 
     @_disfavoredOverload
-    nonisolated
-    init<S, Value>(_ title: S, symbol: Symbol, value: Value?) where Destination == Never, S: StringProtocol, Value: Hashable {
+    nonisolated init<S, Value>(_ title: S, symbol: Symbol, value: Value?) where Destination == Never, S: StringProtocol, Value: Hashable {
         let title = String(title)
         self.init(value: value) {
             SwiftUI.Label(title, symbol: symbol)
@@ -102,8 +89,7 @@ public extension NavigationLink where Label == SwiftUI.Label<Text, Image> {
     }
 
     @_disfavoredOverload
-    nonisolated
-    init<Value>(_ titleResource: LocalizedStringResource, symbol: Symbol, value: Value?) where Destination == Never, Value: Hashable {
+    nonisolated init<Value>(_ titleResource: LocalizedStringResource, symbol: Symbol, value: Value?) where Destination == Never, Value: Hashable {
         self.init(value: value) {
             SwiftUI.Label(titleResource, symbol: symbol)
         }
@@ -140,96 +126,81 @@ public extension Link where Label == SwiftUI.Label<Text, Image> {
 }
 
 public extension Menu where Label == SwiftUI.Label<Text, Image> {
-    nonisolated
-    init(_ title: LocalizedStringKey, symbol: Symbol, @ViewBuilder content: () -> Content) {
+    nonisolated init(_ title: LocalizedStringKey, symbol: Symbol, @ViewBuilder content: () -> Content) {
         self.init(title, systemImage: symbol.rawValue, content: content)
     }
 
     @_disfavoredOverload
-    nonisolated
-    init<S>(_ title: S, symbol: Symbol, @ViewBuilder content: () -> Content) where S: StringProtocol {
+    nonisolated init<S>(_ title: S, symbol: Symbol, @ViewBuilder content: () -> Content) where S: StringProtocol {
         self.init(String(title), systemImage: symbol.rawValue, content: content)
     }
 
     @_disfavoredOverload
-    nonisolated
-    init(_ titleResource: LocalizedStringResource, symbol: Symbol, @ViewBuilder content: () -> Content) {
+    nonisolated init(_ titleResource: LocalizedStringResource, symbol: Symbol, @ViewBuilder content: () -> Content) {
         self.init(titleResource, systemImage: symbol.rawValue, content: content)
     }
 }
 
 public extension Picker where Label == SwiftUI.Label<Text, Image> {
-    nonisolated
-    init(_ title: LocalizedStringKey, symbol: Symbol, selection: Binding<SelectionValue>, @ViewBuilder content: () -> Content) {
+    nonisolated init(_ title: LocalizedStringKey, symbol: Symbol, selection: Binding<SelectionValue>, @ViewBuilder content: () -> Content) {
         self.init(title, systemImage: symbol.rawValue, selection: selection, content: content)
     }
 
     @_disfavoredOverload
-    nonisolated
-    init<S>(_ title: S, symbol: Symbol, selection: Binding<SelectionValue>, @ViewBuilder content: () -> Content) where S: StringProtocol {
+    nonisolated init<S>(_ title: S, symbol: Symbol, selection: Binding<SelectionValue>, @ViewBuilder content: () -> Content) where S: StringProtocol {
         self.init(String(title), systemImage: symbol.rawValue, selection: selection, content: content)
     }
 
     @_disfavoredOverload
-    nonisolated
-    init(_ titleResource: LocalizedStringResource, symbol: Symbol, selection: Binding<SelectionValue>, @ViewBuilder content: () -> Content) {
+    nonisolated init(_ titleResource: LocalizedStringResource, symbol: Symbol, selection: Binding<SelectionValue>, @ViewBuilder content: () -> Content) {
         self.init(titleResource, systemImage: symbol.rawValue, selection: selection, content: content)
     }
 }
 
 public extension ControlGroup {
-    nonisolated
-    init<Controls>(_ title: LocalizedStringKey, symbol: Symbol, @ViewBuilder content: () -> Controls) where Content == LabeledControlGroupContent<Controls, SwiftUI.Label<Text, Image>>, Controls: View {
+    nonisolated init<Controls>(_ title: LocalizedStringKey, symbol: Symbol, @ViewBuilder content: () -> Controls) where Content == LabeledControlGroupContent<Controls, SwiftUI.Label<Text, Image>>, Controls: View {
         self.init(title, systemImage: symbol.rawValue, content: content)
     }
 
     @_disfavoredOverload
-    nonisolated
-    init<S, Controls>(_ title: S, symbol: Symbol, @ViewBuilder content: () -> Controls) where Content == LabeledControlGroupContent<Controls, SwiftUI.Label<Text, Image>>, S: StringProtocol, Controls: View {
+    nonisolated init<S, Controls>(_ title: S, symbol: Symbol, @ViewBuilder content: () -> Controls) where Content == LabeledControlGroupContent<Controls, SwiftUI.Label<Text, Image>>, S: StringProtocol, Controls: View {
         self.init(String(title), systemImage: symbol.rawValue, content: content)
     }
 
     @_disfavoredOverload
-    nonisolated
-    init<Controls>(_ titleResource: LocalizedStringResource, symbol: Symbol, @ViewBuilder content: () -> Controls) where Content == LabeledControlGroupContent<Controls, SwiftUI.Label<Text, Image>>, Controls: View {
+    nonisolated init<Controls>(_ titleResource: LocalizedStringResource, symbol: Symbol, @ViewBuilder content: () -> Controls) where Content == LabeledControlGroupContent<Controls, SwiftUI.Label<Text, Image>>, Controls: View {
         self.init(titleResource, systemImage: symbol.rawValue, content: content)
     }
 }
 
 public extension ContentUnavailableView where Label == SwiftUI.Label<Text, Image>, Description == Text?, Actions == EmptyView {
-    nonisolated
-    init(_ title: LocalizedStringKey, symbol: Symbol, description: Text? = nil) {
+    nonisolated init(_ title: LocalizedStringKey, symbol: Symbol, description: Text? = nil) {
         self.init(title, systemImage: symbol.rawValue, description: description)
     }
 
     @_disfavoredOverload
-    nonisolated
-    init<S>(_ title: S, symbol: Symbol, description: Text? = nil) where S: StringProtocol {
+    nonisolated init<S>(_ title: S, symbol: Symbol, description: Text? = nil) where S: StringProtocol {
         self.init(String(title), systemImage: symbol.rawValue, description: description)
     }
 
     @_disfavoredOverload
-    nonisolated
-    init(_ titleResource: LocalizedStringResource, symbol: Symbol, description: Text? = nil) {
+    nonisolated init(_ titleResource: LocalizedStringResource, symbol: Symbol, description: Text? = nil) {
         self.init(titleResource, systemImage: symbol.rawValue, description: description)
     }
 }
 
 public extension Toggle where Label == SwiftUI.Label<Text, Image> {
-    nonisolated
-    init(_ titleKey: LocalizedStringKey, symbol: Symbol, isOn: Binding<Bool>) {
+    nonisolated init(_ titleKey: LocalizedStringKey, symbol: Symbol, isOn: Binding<Bool>) {
         self.init(titleKey, systemImage: symbol.rawValue, isOn: isOn)
     }
 
     @_disfavoredOverload
-    nonisolated
-    init<S>(_ title: S, symbol: Symbol, isOn: Binding<Bool>) where S: StringProtocol {
+    nonisolated init<S>(_ title: S, symbol: Symbol, isOn: Binding<Bool>) where S: StringProtocol {
         self.init(String(title), systemImage: symbol.rawValue, isOn: isOn)
     }
 
     @_disfavoredOverload
-    nonisolated
-    init(_ titleResource: LocalizedStringResource, symbol: Symbol, isOn: Binding<Bool>) {
+    nonisolated init(_ titleResource: LocalizedStringResource, symbol: Symbol, isOn: Binding<Bool>) {
         self.init(titleResource, systemImage: symbol.rawValue, isOn: isOn)
     }
 }
